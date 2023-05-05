79d7d15625ce5d05f19033d8890f0691a32756c1
Ivan Storonsky <jstoronsky@gmail.com>
2023-05-06 03:53:16 +1200

    hw 15.1 _2nd_attempt

--- src/item.py
+++ src/item.py
@@ -38,7 +38,7 @@ class Item:
 
     @property
     def name(self):
-        return self.__name.title()
+        return self.__name
 
     @name.setter
     def name(self, name):
--- src/phone.py
+++ src/phone.py
@@ -1,10 +1,21 @@
-from item import Item
+from src.item import Item
 
 
 class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, sims):
+        if sims > 0:
+            self.__number_of_sim = sims
+        else:
+            raise ValueError(f"Количество физических SIM-карт должно быть целым числом больше нуля")
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
